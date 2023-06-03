485d84b65812836deb773ef35baf74fc53abfbb6
Chritelle-12 <mariechristellenirere@gmail.com>
2023-06-03 08:27:49 +0200

    Made changes requested

--- src/tests/Detail.test.js
+++ src/tests/Detail.test.js
@@ -3,8 +3,30 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import Detail from '../components/Detail';
+import newsReducer, { incrementView } from '../redux/Home/newsSlice';
 
 const mockStore = configureMockStore();
+describe('newsSlice', () => {
+  test('should increment the view count when incrementView action is dispatched', () => {
+    const initialState = {
+      news: [
+        {
+          title: 'News 1',
+          views: 0,
+        },
+        {
+          title: 'News 2',
+          views: 0,
+        },
+      ],
+    };
+
+    const newState = newsReducer(initialState, incrementView({ newsItemId: 'News 1' }));
+
+    expect(newState.news[0].views).toBe(1);
+    expect(newState.news[1].views).toBe(0);
+  });
+});
 
 describe('Detail Component', () => {
   let store;
